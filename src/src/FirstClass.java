21760fe94cc4c1ac473c80db497a97cace2340ad
usonictw <usonictw@bigmir.net>
2016-08-14 12:43:09 +0300

    My first five Classes in Java

--- src/src/FirstClass.java
+++ src/src/FirstClass.java
@@ -1,8 +1,7 @@
-/**
- * Created by PC on 14.08.2016.
- */
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("My first Class in Java");
+        System.out.print("Hell...");
+        System.out.print("My first class");
     }
 }
